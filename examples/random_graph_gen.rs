47a59427b0893d1c47f95b6668f7daaffa6a5ae4
Tim Beurskens <tim.beurskens@solarteameindhoven.nl>
2022-03-09 16:01:31 +0100

    generate graphs for coloring

--- examples/random_graph_gen.rs
+++ examples/random_graph_gen.rs
@@ -2,6 +2,7 @@
 extern crate clap;
 
 use rand::seq::SliceRandom;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::Write;
@@ -17,13 +18,14 @@ fn main() -> io::Result<()> {
         (@arg output: -o --output +takes_value "The output file")
         (@arg undirected: -u --undirected !takes_value "Use undirected edges (test for both directions in the set complement operation)")
         (@arg dot: -d --dot !takes_value "Output in dot format")
-        (@arg convert: -c --convert +takes_value "Do not generate a new graph, but convert an existing edge list")
+        (@arg convert: -i --input +takes_value "Do not generate a new graph, but convert an existing edge list")
+        (@arg colors: -c --colors +takes_value "Convert the graph to a graph-coloring specification")
     )
     .get_matches();
 
     let undirected = args.is_present("undirected");
 
-    let selection = if args.is_present("convert") {
+    let mut selection = if args.is_present("convert") {
         let file = File::open(args.value_of("convert").unwrap()).expect("Could not open file");
         let mut bufreader = BufReader::new(file);
         read_graph(&mut bufreader, undirected).expect("Could not parse edge list")
@@ -41,6 +43,17 @@ fn main() -> io::Result<()> {
 
         generate_graph(num_vertices, num_edges, undirected)
     };
+
+    // convert to a graph-coloring problem
+    if args.is_present("colors") {
+        let num_colors = args
+            .value_of("colors")
+            .unwrap()
+            .parse::<usize>()
+            .expect("Colors is not a valid number");
+
+        selection = augment_colors(&selection, num_colors);
+    }
 
     let output = args.value_of("output");
 
@@ -126,4 +139,47 @@ fn generate_graph(
     edges.shuffle(&mut rng);
 
     edges[0..num_edges].to_vec()
+}
+
+fn augment_colors(edges: &Vec<(String, String)>, num_colors: usize) -> Vec<(String, String)> {
+    let mut vertex_map: HashMap<String, String> = HashMap::new();
+    let mut color_map: HashMap<String, usize> = HashMap::new();
+
+    for edge in edges {
+        for color in 0..num_colors {
+            let v1 = format!("{}_c{}", edge.0, color);
+            let v2 = format!("{}_c{}", edge.1, color);
+
+            vertex_map.insert(v1.clone(), edge.0.clone());
+            vertex_map.insert(v2.clone(), edge.1.clone());
+
+            color_map.insert(v1, color);
+            color_map.insert(v2, color);
+        }
+    }
+
+    let mut new_edges: Vec<(String, String)> = Vec::new();
+
+    let vertices = vertex_map.keys().cloned().collect::<Vec<String>>();
+
+    for (i, v1) in vertices.iter().enumerate() {
+        for v2 in vertices[(i + 1)..].iter() {
+            let c1 = color_map[v1];
+            let c2 = color_map[v2];
+
+            let ov1 = &vertex_map[v1];
+            let ov2 = &vertex_map[v2];
+
+            // only add an edge to new_edges if the colors are different, or the vertices are not connected
+            if ov1 != ov2
+                && (c1 != c2
+                    || (!edges.contains(&(ov1.clone(), ov2.clone()))
+                        && !edges.contains(&(ov2.clone(), ov1.clone()))))
+            {
+                new_edges.push((v1.clone(), v2.clone()));
+            }
+        }
+    }
+
+    new_edges
 }
