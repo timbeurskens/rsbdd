2ff39e286d8be0f9baf285ed9c1fa6399cdf08c3
Tim Beurskens <tim.beurskens@solarteameindhoven.nl>
2022-03-09 14:47:25 +0100

    implement graph conversion

--- examples/random_graph_gen.rs
+++ examples/random_graph_gen.rs
@@ -16,22 +16,90 @@ fn main() -> io::Result<()> {
         (@arg edges: -e --edges +takes_value "Number of edges")
         (@arg output: -o --output +takes_value "The output file")
         (@arg undirected: -u --undirected !takes_value "Use undirected edges (test for both directions in the set complement operation)")
+        (@arg dot: -d --dot !takes_value "Output in dot format")
+        (@arg convert: -c --convert +takes_value "Do not generate a new graph, but convert an existing edge list")
     )
     .get_matches();
 
-    let num_vertices = args
-        .value_of("vertices")
-        .expect("Specify the number of vertices")
-        .parse::<usize>()
-        .unwrap();
-    let num_edges = args
-        .value_of("edges")
-        .expect("Specify the number of edges")
-        .parse::<usize>()
-        .unwrap();
-
     let undirected = args.is_present("undirected");
 
+    let selection = if args.is_present("convert") {
+        let file = File::open(args.value_of("convert").unwrap()).expect("Could not open file");
+        let mut bufreader = BufReader::new(file);
+        read_graph(&mut bufreader).expect("Could not parse edge list")
+    } else {
+        let num_vertices = args
+            .value_of("vertices")
+            .expect("Specify the number of vertices")
+            .parse::<usize>()
+            .unwrap();
+        let num_edges = args
+            .value_of("edges")
+            .expect("Specify the number of edges")
+            .parse::<usize>()
+            .unwrap();
+
+        generate_graph(num_vertices, num_edges, undirected)
+    };
+
+    let output = args.value_of("output");
+
+    let mut writer = if output.is_some() {
+        let file = File::create(output.unwrap())?;
+        Box::new(BufWriter::new(file)) as Box<dyn Write>
+    } else {
+        Box::new(BufWriter::new(io::stdout())) as Box<dyn Write>
+    };
+
+    if args.is_present("dot") {
+        if undirected {
+            writeln!(writer, "graph G {{")?;
+            for edge in selection {
+                writeln!(writer, "    {} -- {}", edge.0, edge.1)?;
+            }
+            writeln!(writer, "}}")?;
+        } else {
+            writeln!(writer, "digraph G {{")?;
+            for edge in selection {
+                writeln!(writer, "    {} -> {}", edge.0, edge.1)?;
+            }
+            writeln!(writer, "}}")?;
+        }
+    } else {
+        for edge in selection {
+            writeln!(writer, "{},{}", edge.0, edge.1)?;
+        }
+    }
+
+    // flush the writer before dropping it
+    writer.flush().expect("Could not flush write buffer");
+
+    Ok(())
+}
+
+fn read_graph<R: Read>(reader: R) -> io::Result<Vec<(String, String)>> {
+    let mut csv_reader = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(reader);
+
+    let mut edges: Vec<(String, String)> = Vec::new();
+
+    for edge_record in csv_reader.records() {
+        let edge = edge_record?;
+
+        assert!(edge.len() == 2);
+
+        edges.push((edge[0].to_string(), edge[1].to_string()));
+    }
+
+    Ok(edges)
+}
+
+fn generate_graph(
+    num_vertices: usize,
+    num_edges: usize,
+    undirected: bool,
+) -> Vec<(String, String)> {
     let mut rng = rand::thread_rng();
 
     let vertices = (0..num_vertices)
@@ -55,21 +123,5 @@ fn main() -> io::Result<()> {
 
     edges.shuffle(&mut rng);
 
-    let output = args.value_of("output");
-
-    let mut writer = if output.is_some() {
-        let file = File::create(output.unwrap())?;
-        Box::new(BufWriter::new(file)) as Box<dyn Write>
-    } else {
-        Box::new(BufWriter::new(io::stdout())) as Box<dyn Write>
-    };
-
-    for edge in edges[0..num_edges].iter() {
-        writeln!(writer, "{},{}", edge.0, edge.1)?;
-    }
-
-    // flush the writer before dropping it
-    writer.flush().expect("Could not flush write buffer");
-
-    Ok(())
+    edges[0..num_edges].to_vec()
 }
