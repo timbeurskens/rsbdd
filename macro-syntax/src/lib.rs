84a1378abee6d5c5338d0439a992fdb6915bdb3e
Tim Beurskens <tim.beurskens@lightyear.one>
2022-10-17 22:37:58 +0200

    add tests and refactor code

--- macro-syntax/src/lib.rs
+++ macro-syntax/src/lib.rs
@@ -1,20 +1,10 @@
 #[macro_export]
-macro_rules! _bdd_decompose {
-    ($env:ident :: $head:ident) => {
-        $env.var("$head".into())
-    };
-    ($env:ident :: $head:tt & $($tail:tt)*) => {
-        $env.and($crate::_bdd_decompose!($env :: $head), $crate::_bdd_decompose!($env :: $($tail)*))
-    };
-}
-
-#[macro_export]
 macro_rules! bdd {
     ($($expr:tt)+) => {{
-        let new_bddenv = rsbdd::bdd::BDDEnv::<rsbdd::bdd::NamedSymbol>::new();
+        let input = stringify!($($expr)+);
+        let mut input_reader = std::io::BufReader::new(input.as_bytes());
+        let parsed_formula = rsbdd::parser::ParsedFormula::new(&mut input_reader, None).expect("could not parse expression");
 
-        let bdd_expr = $crate::_bdd_decompose!(new_bddenv :: $($expr)+);
-
-        bdd_expr
+        parsed_formula.eval()
     }};
 }
--- macro-syntax/tests/syntax_test.rs
+++ macro-syntax/tests/syntax_test.rs
@@ -2,9 +2,9 @@ use macro_syntax::bdd;
 
 #[test]
 fn test_basic_syntax_1() {
-    let form = bdd! {
-        a & b
-    };
+    let e1 = bdd!(a | -a);
+    let e2 = bdd!(a | b | c);
+    let e3 = bdd!(false);
 
-    print!("{:?}", form);
+    println!("{:#?}\n{:#?}\n{:#?}", e1, e2, e3);
 }
