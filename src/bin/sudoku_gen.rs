f24125835724099ab26001090196d496919a6fc4
Tim Beurskens <t.m.j.a.beurskens@student.tue.nl>
2022-11-30 13:31:11 +0100

    allow smaller sudoku-like puzzles

--- src/bin/sudoku_gen.rs
+++ src/bin/sudoku_gen.rs
@@ -17,12 +17,20 @@ struct Args {
     #[clap(value_parser, value_name = "OUTPUT")]
     /// The output rsbdd file
     output: Option<PathBuf>,
+
+    #[clap(short, long, value_parser, value_name = "N", default_value_t = 3)]
+    /// The root value of the puzzle. Typically the square root of the largest possible number
+    root: usize,
 }
 
 fn main() -> io::Result<()> {
     let version = env!("CARGO_PKG_VERSION");
 
     let args = Args::parse();
+
+    let root = args.root;
+    let square = root * root;
+    let numcells = square * square;
 
     let mut writer = if let Some(output) = args.output {
         let file = File::create(output)?;
@@ -57,7 +65,7 @@ fn main() -> io::Result<()> {
 
     writeln!(writer)?;
 
-    for i in 0..(9 * 9) {
+    for i in 0..numcells {
         if let Some(ch) = puzzle_input.chars().nth(i) {
             if char::is_digit(ch, 10) {
                 writeln!(writer, "_{}_is_{} &", i, ch)?;
@@ -67,12 +75,12 @@ fn main() -> io::Result<()> {
 
     writeln!(writer)?;
 
-    writeln!(writer, "\"each cell can either be 1, 2, .., 9\"")?;
+    writeln!(writer, "\"each cell can either be 1, 2, .., {}\"", square)?;
 
     writeln!(writer)?;
 
-    for i in 0..(9 * 9) {
-        let vars = (1..=9)
+    for i in 0..numcells {
+        let vars = (1..=square)
             .map(|j| format!("_{}_is_{}", i, j))
             .collect::<Vec<_>>()
             .join(", ");
@@ -88,16 +96,16 @@ fn main() -> io::Result<()> {
 
     writeln!(writer)?;
 
-    for i in 0..9 {
-        for k in 1..=9 {
-            let vars = (0..9)
-                .map(|j| format!("_{}_is_{}", i * 9 + j, k))
+    for i in 0..square {
+        for k in 1..=square {
+            let vars = (0..square)
+                .map(|j| format!("_{}_is_{}", i * square + j, k))
                 .collect::<Vec<_>>()
                 .join(", ");
             writeln!(writer, "[{}] = 1 &", vars)?;
 
-            let vars = (0..9)
-                .map(|j| format!("_{}_is_{}", j * 9 + i, k))
+            let vars = (0..square)
+                .map(|j| format!("_{}_is_{}", j * square + i, k))
                 .collect::<Vec<_>>()
                 .join(", ");
             writeln!(writer, "[{}] = 1 &", vars)?;
@@ -110,12 +118,12 @@ fn main() -> io::Result<()> {
 
     writeln!(writer)?;
 
-    for i in 0..3 {
-        for j in 0..3 {
-            let lt = (i * 3) * 9 + (j * 3);
-            for k in 1..=9 {
-                let vars = (0..9)
-                    .map(|l| format!("_{}_is_{}", lt + ((l / 3) * 9 + (l % 3)), k))
+    for i in 0..root {
+        for j in 0..root {
+            let lt = (i * root) * square + (j * root);
+            for k in 1..=square {
+                let vars = (0..square)
+                    .map(|l| format!("_{}_is_{}", lt + ((l / root) * square + (l % root)), k))
                     .collect::<Vec<_>>()
                     .join(", ");
                 writeln!(writer, "[{}] = 1 &", vars)?;
